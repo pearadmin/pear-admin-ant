886a4ff0b45072adfc16454285df2ffb628c12eb
maplemei <meisanfeng@youdi.net>
2020-10-29 14:40:23 +0800

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,7 +9,7 @@
               :value="14.38"
               :precision="2"
               suffix="%"
-              :value-style="{ color: '#3f8600' }"
+              :value-style="{ color: '#cf1322' }"
               style="margin-right: 50px"
             >
               <template v-slot:prefix>
@@ -26,7 +26,7 @@
               :precision="2"
               suffix="%"
               class="demo-class"
-              :value-style="{ color: '#cf1322' }"
+              :value-style="{ color: '#3f8600' }"
             >
               <template v-slot:prefix>
                 <Arrow-down-outlined />
@@ -41,7 +41,7 @@
               :value="15.38"
               :precision="2"
               suffix="%"
-              :value-style="{ color: '#3f8600' }"
+              :value-style="{ color: '#cf1322' }"
               style="margin-right: 50px"
             >
               <template v-slot:prefix>
@@ -58,7 +58,7 @@
               :precision="2"
               suffix="%"
               class="demo-class"
-              :value-style="{ color: '#cf1322' }"
+              :value-style="{ color: '#3f8600' }"
             >
               <template v-slot:prefix>
                 <Arrow-down-outlined />
@@ -68,7 +68,7 @@
         </a-col>
         <a-col :span="18">
           <a-card>
-            <a-card-grid style="width: 33.33%; text-align: center">
+            <a-card-grid style="width: 33.33%; text-align: center" v-for="(item, index) in 3" :key="index">
               <a-card-meta
                 title="Pear Admin"
                 description="慢慢来是一种诚意"
@@ -80,55 +80,7 @@
                 </template>
               </a-card-meta>
             </a-card-grid>
-            <a-card-grid style="width: 33.33%; text-align: center">
-              <a-card-meta
-                title="Pear Admin"
-                description="慢慢来是一种诚意"
-              >
-                <template v-slot:avatar>
-                  <a-avatar
-                    src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
-                  />
-                </template>
-              </a-card-meta>
-            </a-card-grid>
-            <a-card-grid style="width: 33.33%; text-align: center">
-              <a-card-meta
-                title="Pear Admin"
-                description="慢慢来是一种诚意"
-              >
-                <template v-slot:avatar>
-                  <a-avatar
-                    src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
-                  />
-                </template>
-              </a-card-meta>
-            </a-card-grid>
-            <a-card-grid style="width: 33.33%; text-align: center">
-              <a-card-meta
-                title="Pear Admin"
-                description="凛冬将至"
-              >
-                <template v-slot:avatar>
-                  <a-avatar
-                    src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
-                  />
-                </template>
-              </a-card-meta>
-            </a-card-grid>
-            <a-card-grid style="width: 33.33%; text-align: center">
-              <a-card-meta
-                title="Pear Admin"
-                description="凛冬将至"
-              >
-                <template v-slot:avatar>
-                  <a-avatar
-                    src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
-                  />
-                </template>
-              </a-card-meta>
-            </a-card-grid>
-            <a-card-grid style="width: 33.33%; text-align: center">
+            <a-card-grid style="width: 33.33%; text-align: center" v-for="(item, index) in 3" :key="index">
               <a-card-meta
                 title="Pear Admin"
                 description="凛冬将至"
@@ -142,19 +94,17 @@
             </a-card-grid>
           </a-card>
           <a-card title="动态" style="margin-top: 15px">
-            <a-list item-layout="horizontal" :data-source="data">
+            <a-list item-layout="horizontal" :data-source="[1, 2, 3, 4]">
               <template v-slot:renderItem="{ item }">
                 <a-list-item>
                   <a-list-item-meta
                     description="Ant Design, a design language for background applications, is refined by Ant UED Team"
                   >
                     <template v-slot:title>
-                      <a href="https://www.antdv.com/">{{ item.title }}</a>
+                      <a href="https://www.antdv.com/">Pear Title {{ item }}</a>
                     </template>
                     <template v-slot:avatar>
-                      <a-avatar
-                        src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
-                      />
+                      <a-avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
                     </template>
                   </a-list-item-meta>
                 </a-list-item>
@@ -164,19 +114,17 @@
         </a-col>
         <a-col :span="6">
           <a-card>
-            <a-list item-layout="horizontal" :data-source="data">
+            <a-list item-layout="horizontal" :data-source="[1, 2, 3, 4]">
               <template v-slot:renderItem="{ item }">
                 <a-list-item>
                   <a-list-item-meta
                     description="Ant Design, a design language for background applications, is refined by Ant UED Team"
                   >
                     <template v-slot:title>
-                      <a href="https://www.antdv.com/">{{ item.title }}</a>
+                      <a href="https://www.antdv.com/">Pear Title {{ item }}</a>
                     </template>
                     <template v-slot:avatar>
-                      <a-avatar
-                        src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
-                      />
+                      <a-avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
                     </template>
                   </a-list-item-meta>
                 </a-list-item>
@@ -190,29 +138,10 @@
 </template>
 <script>
 import { ArrowUpOutlined, ArrowDownOutlined } from "@ant-design/icons-vue";
-const data = [
-  {
-    title: "Ant Design Title 1",
-  },
-  {
-    title: "Ant Design Title 2",
-  },
-  {
-    title: "Ant Design Title 3",
-  },
-  {
-    title: "Ant Design Title 4",
-  },
-];
 export default {
   components: {
     ArrowUpOutlined,
     ArrowDownOutlined,
-  },
-  data() {
-    return {
-      data,
-    };
   },
 };
 </script>
