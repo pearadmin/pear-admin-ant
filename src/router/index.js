62ed53503bdc18f12c633e83cd752196ba6c0dad
maplemei <meisanfeng@youdi.net>
2020-10-29 14:32:23 +0800

    添加路由切换loading效果

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,27 @@
 import { createRouter,  createWebHashHistory } from 'vue-router'
 import routes from './routes.js'
 
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+
 const router = createRouter({
   // 使用 hash 模式构建路由（ url中带 # 号的那种)
   history: createWebHashHistory(),
   // 使用 history 模式构建路由 （ url 中没有 # 号，但生产环境需要特殊配置）
   // history: createWebHistory(),
   routes
+})
+
+router.beforeEach(async (to, from) => {
+  //配置路由加载动画效果
+  NProgress.start();
+  //这里也可以验证权限
+
+  return true;
+})
+
+router.afterEach((to, from) => {
+  NProgress.done();
 })
 
 export default router
