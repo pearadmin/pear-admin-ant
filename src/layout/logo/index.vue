1f6f8dbe6ab232fd54d9309043f5d0a0c7985ca9
就眠仪式 <854085467@qq.com>
2020-11-11 18:16:45 +0800

    修复部分问题

--- src/layout/logo/index.vue
+++ src/layout/logo/index.vue
@@ -2,15 +2,10 @@
   <div id="logo" :class="[theme]">
     <!-- 根据侧边收缩状态决定隐显内容 -->
     <span v-if="!collapsed">
-    ><img class="image"
-        src="../../assets/image/logo.png"
-    />
+      <img class="image" src="../../assets/image/logo.png" />
     </span>
     <!-- 根据侧边收缩状态决定隐显内容 -->
-    <span v-else
-      ><img class="image"
-        src="../../assets/image/logo.png"
-    /></span>
+    <span v-else><img class="image" src="../../assets/image/logo.png" /></span>
   </div>
 </template>
 <script>
