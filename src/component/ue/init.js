d7a88cdf69ff6352c7d0acadffa8375c85ce72d6
周伟 <>
2021-01-26 14:27:33 +0800

    -tinymce

--- src/component/ue/init.js
+++ src/component/ue/init.js
@@ -1,4 +1,3 @@
-const publicPath = "";
 // imageUpload
 const toolbar = [
   "imageUpload bold italic underline strikethrough table | alignleft aligncenter alignright alignjustify ltr rtl \
@@ -8,12 +7,10 @@ const toolbar = [
 const plugins = [
   "print preview searchreplace autolink directionality visualblocks visualchars fullscreen link media template code codesample table charmap hr pagebreak nonbreaking anchor insertdatetime advlist lists wordcount imagetools textpattern help autosave  autoresize "
 ];
-let baseUrl = publicPath;
 let init = {
-  language_url: `${baseUrl}/tinymce/langs/zh_CN.js`,
+  language_url: require("@/assets/tinymce/langs/zh_CN.js"),
   language: "zh_CN",
-  skin_url: `${baseUrl}/tinymce/skins/ui/oxide`,
-  content_css: `${baseUrl}/tinymce/skins/content/default/content.css`,
+  skin_url: require("@/assets/tinymce/skins/ui/oxide/skin.css"),
   min_height: 300,
   plugins: plugins,
   toolbar: toolbar,
