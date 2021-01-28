32bff47fada09d33a66b96f60894cd7763fb56d2
周伟 <>
2021-01-28 14:52:03 +0800

    -

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@
  * @type {{devServer: {port: number, open: boolean}}}
  */
 const path = require("path");
-const resolve = dir => {
+const resolve = (dir) => {
   return path.join(__dirname, dir);
 };
 const isDev = process.env.NODE_ENV === "development";
@@ -14,7 +14,7 @@ module.exports = {
   outputDir: "dist",
   devServer: {
     open: true, // 启动后自动打开浏览器
-    port: 8080 // 端口
+    port: 8080, // 端口
   },
   // 构建时开启多进程处理babel编译
   parallel: require("os").cpus().length > 1,
@@ -23,13 +23,20 @@ module.exports = {
     return {
       resolve: {
         alias: {
-          "@": resolve("src")
-        }
-      }
+          "@": resolve("src"),
+        },
+      },
     };
   },
   chainWebpack(config) {
-    config.when(!isDev, config => {
+    config.when(isDev, (config) => {
+      config.devtool("source-map");
+    });
+
+    config.plugins.delete("prefetch");
+    config.resolve.symlinks(true);
+
+    config.when(!isDev, (config) => {
       //splitChunks拆分
       config.optimization.splitChunks({
         chunks: "all",
@@ -38,14 +45,14 @@ module.exports = {
             name: "Chunk-Libs",
             test: /[\\/]node_modules[\\/]/,
             priority: 10,
-            chunks: "initial"
+            chunks: "initial",
           },
           AntdUI: {
             name: "Chunk-Antd",
             priority: 20,
-            test: /[\\/]node_modules[\\/]_?@ant-design(.*)/
-          }
-        }
+            test: /[\\/]node_modules[\\/]_?@ant-design(.*)/,
+          },
+        },
       });
       //Gzip
       config
@@ -58,10 +65,10 @@ module.exports = {
               "\\.(" + ProductionGzipExtensions.join("|") + ")$"
             ),
             threshold: 8192,
-            minRatio: 0.8
-          }
+            minRatio: 0.8,
+          },
         ])
         .end();
     });
-  }
+  },
 };
