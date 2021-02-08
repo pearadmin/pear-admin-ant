576421fce6d473de257c333e6078ea009f0af4b1
周伟 <>
2021-02-08 09:50:56 +0800

    添加错误页面返回首页倒计时

--- src/view/error/500.vue
+++ src/view/error/500.vue
@@ -2,13 +2,12 @@
   <div id="500">
     <page-layout>
       <a-card>
-        <a-result
-          status="500"
-          title="500"
-          sub-title="Sorry, the server is wrong."
-        >
+        <a-result status="500" title="500" sub-title="服务器开小差了">
           <template #extra>
-            <a-button type="primary"> Back Home </a-button>
+            <div style="margin-bottom:20px">{{ overTime }} 秒后返回首页</div>
+            <router-link to="/">
+              <a-button type="primary"> 返回首页 </a-button>
+            </router-link>
           </template>
         </a-result>
       </a-card>
@@ -16,3 +15,22 @@
     <page-footer></page-footer>
   </div>
 </template>
+<script>
+import { ref, onUnmounted } from "vue";
+import router from "@/router";
+export default {
+  setup() {
+    const overTime = ref(5);
+    setTimeout(() => {
+      router.push("/");
+    }, overTime.value * 1000);
+    const Interval = setInterval(() => {
+      overTime.value--;
+    }, 1000);
+    onUnmounted(() => {
+      clearInterval(Interval);
+    });
+    return { overTime };
+  }
+};
+</script>
