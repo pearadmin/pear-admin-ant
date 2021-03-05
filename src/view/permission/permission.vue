42502685fae7b875da50983201d8d33573dbf02d
就眠仪式 <854085467@qq.com>
2021-03-06 02:31:12 +0800

    edit layout size

--- src/view/permission/permission.vue
+++ src/view/permission/permission.vue
@@ -5,36 +5,42 @@
       describe="根 据 权 限 提 供 节 点 的 渲 染"
     ></page-header>
     <page-layout>
-      <a-card>
-        <h2>权限按钮组(不同的用户进来看到的按钮不一样)</h2>
-        <h3>当前用户{{useInfo?.permissions[0]?.role}}</h3>
-        <a-space>
-          <a-button type="primary" v-permission:add>增</a-button>
-          <a-button type="primary" v-permission:del>删</a-button>
-          <a-button type="primary" v-permission:upd>改</a-button>
-        </a-space>
-      </a-card>
+      <a-row :gutter="[10,10]">
+        <a-col :span="24">
+          <a-card>
+            <h3>[ {{ useInfo?.permissions[0]?.role }} ]</h3>
+          </a-card>
+        </a-col>
+        <a-col :span="24">
+          <a-card>
+            <a-space>
+              <a-button type="primary" v-permission:add>增</a-button>
+              <a-button type="primary" v-permission:del>删</a-button>
+              <a-button type="primary" v-permission:upd>改</a-button>
+            </a-space>
+          </a-card>
+        </a-col>
+      </a-row>
     </page-layout>
     <page-footer></page-footer>
   </div>
 </template>
 
 <script>
-import {computed, defineComponent} from "vue";
-import {useStore} from "vuex";
+import { computed, defineComponent } from "vue";
+import { useStore } from "vuex";
 
 export default defineComponent({
-  name: 'permission',
-  setup () {
-    const store = useStore()
-    const useInfo = computed(() => store.state.user.userInfo)
+  name: "permission",
+  setup() {
+    const store = useStore();
+    const useInfo = computed(() => store.state.user.userInfo);
     return {
-      useInfo
-    }
-  }
-})
+      useInfo,
+    };
+  },
+});
 </script>
 
 <style scoped lang="less">
-
 </style>
