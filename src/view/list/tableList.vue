dae5626aeebda216b76f09f0691479d041d8b6b2
就眠儀式 <854085467@qq.com>
2021-11-21 23:06:37 +0800

    ✅ 修复因包版本产生的所有警告

--- src/view/list/tableList.vue
+++ src/view/list/tableList.vue
@@ -5,16 +5,16 @@
       <a-card>
         <p-table
           :fetch="fetch"
-          :value='obj'
+          :value="obj"
           :columns="columns"
           :toolbar="toolbar"
-          :operate ="operate"
+          :operate="operate"
           :pagination="pagination"
         >
-        <!-- 继承至 a-table 的默认插槽 -->
-        <template #name="{ record }">
-          {{record.name}}
-        </template>
+          <!-- 继承至 a-table 的默认插槽 -->
+          <template #name="{ record }">
+            {{ record.name }}
+          </template>
         </p-table>
       </a-card>
     </page-layout>
@@ -22,7 +22,6 @@
   </div>
 </template>
 <script>
-
 const dataItem = {
   key: "1",
   name: "Joe Black",
@@ -30,53 +29,107 @@ const dataItem = {
   age: 32,
   createTime: "2020-02-09 00:00:00",
   address: "Sidney No. 1 Lake Park Sidney No. 1 ",
-  tags: ["cool", "teacher"]
+  tags: ["cool", "teacher"],
 };
 
 export default {
   setup() {
-
     /// 数据来源 [模拟]
-    const fetch = async param => {
-      return new Promise(resolve => {
+    const fetch = async (param) => {
+      return new Promise((resolve) => {
         setTimeout(() => {
           resolve({
             total: 100,
-            data: new Array(param.pageSize).fill(dataItem)
+            data: new Array(param.pageSize).fill(dataItem),
           });
-        },900);
+        }, 900);
       });
     };
 
     /// 工具栏
     const toolbar = [
-      {label: "新增",event: function(keys){alert("新增操作:" + JSON.stringify(keys))}},
-      {label: "删除",event: function(keys){alert("批量删除:" + JSON.stringify(keys))}},
-      {label: "更多操作",children: [
-        {label: "批量导入",event(keys){alert("批量导入")}},
-        {label: "批量导出",event(keys){alert("批量导出")}}
-      ]}
-    ]
+      {
+        label: "新增",
+        event: function (keys) {
+          alert("新增操作:" + JSON.stringify(keys));
+        },
+      },
+      {
+        label: "删除",
+        event: function (keys) {
+          alert("批量删除:" + JSON.stringify(keys));
+        },
+      },
+      {
+        label: "更多操作",
+        children: [
+          {
+            label: "批量导入",
+            event(keys) {
+              alert("批量导入");
+            },
+          },
+          {
+            label: "批量导出",
+            event(keys) {
+              alert("批量导出");
+            },
+          },
+        ],
+      },
+    ];
 
     /// 字段
     const columns = [
-      {title: "姓名", dataIndex: "name", key: "name",slots: { customRender: 'name' }},
-      {title: "性别", dataIndex: "sex", key: "sex"},
-      {title: "年龄", dataIndex: "age", key: "age"},
-      {title: "地址", dataIndex: "address", key: "address"}
+      {
+        title: "姓名",
+        dataIndex: "name",
+        key: "name",
+        slots: { customRender: "name" },
+      },
+      { title: "性别", dataIndex: "sex", key: "sex" },
+      { title: "年龄", dataIndex: "age", key: "age" },
+      { title: "地址", dataIndex: "address", key: "address" },
     ];
 
     /// 行操作
     const operate = [
-      {label: "查看",event: function(record){alert("查看详情:" + JSON.stringify(record));}},
-      {label: "修改",event: function(record){alert("修改事件:" + JSON.stringify(record));}},
-      {label: "删除",event: function(record){alert("删除事件:" + JSON.stringify(record));}},
-      {label: "更多",children: [ 
-        {label: "导出",event: function(record){alert("导出")}},
-        {label: "下载",event: function(record){alert("下载")}}
-      ]}
-
-    ]
+      {
+        label: "查看",
+        event: function (record) {
+          alert("查看详情:" + JSON.stringify(record));
+        },
+      },
+      {
+        label: "修改",
+        event: function (record) {
+          alert("修改事件:" + JSON.stringify(record));
+        },
+      },
+      {
+        label: "删除",
+        event: function (record) {
+          alert("删除事件:" + JSON.stringify(record));
+        },
+      },
+      {
+        label: "更多",
+        children: [
+          {
+            label: "导出",
+            event: function (record) {
+              alert("导出");
+            },
+          },
+          {
+            label: "下载",
+            event: function (record) {
+              alert("下载");
+            },
+          },
+        ],
+      },
+    ];
 
     /// 声明抛出
     return {
@@ -84,8 +137,8 @@ export default {
       fetch: fetch, // 数据回调
       toolbar: toolbar, // 工具栏
       columns: columns, // 列配置
-      operate: operate  // 行操作
+      operate: operate, // 行操作
     };
-  }
+  },
 };
 </script>
