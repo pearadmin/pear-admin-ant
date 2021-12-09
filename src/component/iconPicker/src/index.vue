284dd5564bddbb1d99525c4448aa285cc7466fc6
就眠儀式 <854085467@qq.com>
2021-12-09 03:20:21 +0000

    📢 规范 p-icon-picker 组件

--- src/component/iconPicker/src/index.vue
+++ src/component/iconPicker/src/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="icon-picker">
+  <div class="p-icon-picker">
     <a-dropdown>
       <a-input v-model:value="selected" :name="name" readonly>
         <template #addonAfter>
@@ -7,7 +7,7 @@
         </template>
       </a-input>
       <template #overlay>
-        <a-menu style="height:300px;overflow-y:scroll">
+        <a-menu class="p-icon-picker-content">
           <a-menu-item :key="icon" v-for="icon in icons" @click="select(icon)">
             <p-icon :type="icon"></p-icon>
           </a-menu-item>
@@ -17,40 +17,57 @@
   </div>
 </template>
 <script>
-import icons from './index.js'
-import { watch, ref } from 'vue';
+import icons from "./index.js";
+import { watch, ref } from "vue";
 
 export default {
-    name: 'p-icon-picker',
-    props: {
-      modelValue: {
-        type: String,
-        require: false
-      },
-      name: {
-        type: String,
-        require: false
-      }
+  name: "p-icon-picker",
+  props: {
+    modelValue: {
+      type: String,
+      require: false,
     },
-    setup(props,context) {
-
-        const selected = ref(props.modelValue ? props.modelValue : icons[0]);
-
-        const select = function(icon){
-          selected.value = icon;
-          context.emit('update:modelValue', icon)
-        }
-
-        watch(props,(props) => {
-          selected.value = props.modelValue;
-        })
-
-        return {
-            icons,
-            select,
-            selected
-        }
-
+    name: {
+      type: String,
+      require: false
     }
-}
+  },
+  setup(props, context) {
+    const selected = ref(props.modelValue ? props.modelValue : icons[0]);
+
+    const select = function (icon) {
+      selected.value = icon;
+      context.emit("update:modelValue", icon);
+    };
+
+    watch(props, (props) => {
+      selected.value = props.modelValue;
+    });
+
+    return {
+      icons,
+      select,
+      selected,
+    };
+  },
+};
 </script>
+
+<style>
+.p-icon-picker-content {
+  height: 300px;
+  overflow-y: scroll;
+  width: 310px;
+}
+
+.p-icon-picker-content .ant-dropdown-menu-item {
+  display: inline-block;
+}
+
+.p-icon-picker-content
+  .ant-dropdown-menu-item
+  .ant-dropdown-menu-title-content
+  span {
+  margin-right: 0px !important;
+}
+</style>
